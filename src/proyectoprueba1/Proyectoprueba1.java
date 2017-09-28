3a4bb4eedbedea87c3ea3df93f2de3cd7fd5f852
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-09-28 09:40:00 +0200

    Inicial

--- src/proyectoprueba1/Proyectoprueba1.java
+++ src/proyectoprueba1/Proyectoprueba1.java
@@ -17,6 +17,7 @@ public class Proyectoprueba1 {
         public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Hola");
                        
     }
     
